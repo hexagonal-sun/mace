3d2a7b1397ea886ecd9dc0fe6fc99020d447f31b
Matthew Leach <matthew@mattleach.net>
2018-12-15 18:35:48 +0000

    move: new
    Add a new class that defines a move.

---
+++ src/move.hpp
@@ -0,0 +1,43 @@
+#pragma once
+
+#include <boost/container/small_vector.hpp>
+#include "locus.h"
+#include "piece.h"
+
+enum class MoveType
+{
+    UNOCCUPIED,
+    CASTLE,
+    ENPASSANT_TAKE,
+    TAKE
+};
+
+class Move
+{
+public:
+    Move()
+        : isValid_(false) {};
+
+    Move(Locus from, Locus to, MoveType t)
+        : from_(from), to_(to), type_(t), isValid_(true) {};
+
+    const Locus &getFrom() const { return from_; };
+    const Locus &getTo() const { return to_; };
+    const MoveType getType() const { return type_; };
+    const bool isValid() const { return isValid_; };
+
+private:
+    bool isValid_;
+    Locus from_;
+    Locus to_;
+    MoveType type_;
+};
+
+static inline std::ostream &operator<<(std::ostream &os, const Move &l)
+{
+    os << l.getFrom() << l.getTo();
+
+    return os;
+}
+
+typedef boost::container::small_vector<Move, 64> moveList_t;
