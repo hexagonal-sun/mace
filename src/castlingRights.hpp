869db99a3a24dd1fd44851eb21c308fb92df3238
Matthew Leach <matthew@mattleach.net>
2018-12-16 22:42:44 +0000

    castlingRights:getCastlingMask,getKingSideMask,getQueenSideMask: new
    Add new functions that allow easy masking of the castling rights
    bitset.

--- src/castlingRights.hpp
+++ src/castlingRights.hpp
@@ -1,9 +1,33 @@
 #pragma once
 #include <bitset>
 
+#include "piece.h"
+
 auto constexpr WhiteKingSideBit  = 0;
 auto constexpr BlackKingSideBit  = 1;
 auto constexpr WhiteQueenSideBit = 2;
 auto constexpr BlackQueenSideBit = 3;
 
 typedef std::bitset<4> CastlingRights;
+
+constexpr CastlingRights getCastlingMask(Colour c)
+{
+    if (c == Colour::WHITE)
+        return CastlingRights((1 << WhiteKingSideBit) |
+                              (1 << WhiteQueenSideBit));
+    else
+        return CastlingRights((1 << BlackKingSideBit) |
+                              (1 << BlackQueenSideBit));
+}
+
+constexpr CastlingRights getKingSideMask()
+{
+    return CastlingRights((1 << BlackKingSideBit) |
+                          (1 << WhiteKingSideBit));
+}
+
+constexpr CastlingRights getQueenSideMask()
+{
+    return CastlingRights((1 << BlackQueenSideBit) |
+                          (1 << WhiteQueenSideBit));
+}
