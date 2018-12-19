aa3a852c5e54324e3328d9aaa0c85570181d95fb
Matthew Leach <matthew@mattleach.net>
2018-12-19 11:41:36 +0000

    chessboard: RayIterator: new
    Add a new templated class that represents ray iteration over a chess
    board.

--- src/chessBoard.h
+++ src/chessBoard.h
@@ -1,4 +1,5 @@
-
+#include <list>
+#include <stack>
 #include <array>
 
 #include "boardSquare.h"
@@ -64,6 +65,123 @@ public:
         T *ptr_;
         int rankCount_;
         uint8_t curIdx_;
+    };
+
+    template <typename ret_type,
+              typename chessboard_type,
+              Ray ray_type>
+    class RayIterator
+    {
+        friend class ChessBoard;
+    public:
+        ret_type &operator*()
+            {
+                return cb_[curLoc_];
+            }
+
+        ret_type &operator->()
+            {
+                return cb_[curLoc_];
+            }
+
+        void operator++()
+            {
+                if (ray_type == Ray::KNIGHT)
+                    curLoc_ = originalLoc_;
+
+            nextDir:;
+                if (dirs_.empty()) {
+                    curLoc_ = Locus();
+                    return;
+                }
+
+                for (auto dir : dirs_.top()) {
+                    curLoc_ = curLoc_.translate(dir);
+
+                    if (!curLoc_.isValid()) {
+
+                        dirs_.pop();
+                        curLoc_ = originalLoc_;
+
+                        goto nextDir;
+                    }
+                }
+
+                // For knight rays, we only want to apply the
+                // transformation once.
+                if (ray_type == Ray::KNIGHT)
+                    dirs_.pop();
+            }
+        bool operator==(const ChessBoard::RayIterator<ret_type,
+                                                      chessboard_type,
+                                                      ray_type> &other)
+            {
+                return curLoc_ == other.curLoc_;
+            }
+
+        bool operator!=(const ChessBoard::RayIterator<ret_type,
+                                                      chessboard_type,
+                                                      ray_type> &other)
+            {
+                return (!(curLoc_ == other.curLoc_));
+            }
+
+        ChessBoard::RayIterator<ret_type, chessboard_type, ray_type> &begin()
+            {
+                return *this;
+            }
+
+        ChessBoard::RayIterator<ret_type, chessboard_type, ray_type> end()
+            {
+                return ChessBoard::RayIterator<ret_type,
+                                               chessboard_type,
+                                               ray_type>(cb_,
+                                                         Locus());
+            }
+    private:
+        RayIterator(chessboard_type &b, Locus l)
+            : curLoc_(l),
+              cb_(b),
+              originalLoc_(l)
+            {
+                switch (ray_type) {
+                case Ray::ORTHOGONAL:
+                    dirs_ = std::stack<std::list<Direction>>({
+                            {Direction::NORTH},
+                            {Direction::EAST},
+                            {Direction::SOUTH},
+                            {Direction::WEST}
+                        });
+                    break;
+                case Ray::DIAGONAL:
+                    dirs_ = std::stack<std::list<Direction>>({
+                            {Direction::NORTH, Direction::EAST},
+                            {Direction::NORTH, Direction::WEST},
+                            {Direction::SOUTH, Direction::EAST},
+                            {Direction::SOUTH, Direction::WEST}
+                        });
+                    break;
+                case Ray::KNIGHT:
+                    dirs_ = std::stack<std::list<Direction>>({
+                            {Direction::NORTH, Direction::NORTH, Direction::EAST},
+                            {Direction::NORTH, Direction::NORTH, Direction::WEST},
+                            {Direction::EAST, Direction::EAST, Direction::NORTH},
+                            {Direction::EAST, Direction::EAST, Direction::SOUTH},
+                            {Direction::SOUTH, Direction::SOUTH, Direction::EAST},
+                            {Direction::SOUTH, Direction::SOUTH, Direction::WEST},
+                            {Direction::WEST, Direction::WEST, Direction::NORTH},
+                            {Direction::WEST, Direction::WEST, Direction::SOUTH}
+                        });
+                    break;
+                default:
+                    throw std::invalid_argument("Unknown ray type");
+                }
+            };
+        const Locus originalLoc_;
+        Ray rayType_;
+        Locus curLoc_;
+        chessboard_type &cb_;
+        std::stack<std::list<Direction>> dirs_;
     };
 
     ChessBoard::iterator<BoardSquare> begin();
