a3f16a5f329204d69207e8735ef417dc549865ed
Matthew Leach <matthew@mattleach.net>
2018-12-20 19:05:56 +0000

    search: alphaBeta: use templates for min-max parts
    Use templates to reduce the code duplication in the alpha beta
    function.

--- src/search.cpp
+++ src/search.cpp
@@ -6,64 +6,62 @@
 #include "mover.hpp"
 #include "search.h"
 
+enum class MinMax {
+    MIN,
+    MAX
+};
+
+
+template <MinMax type>
 static int alphaBeta(Board &node, size_t depth,
                      int alpha, int beta,
-                     bool maximizePlayer,
                      Move *bestMove)
 
 {
     if (depth == 0)
         return node.getEvaluation();
 
-    if (maximizePlayer) {
-        int val = -INT_MAX;
+    const auto &moves = node.getAllCandidateMoves();
 
-        for (const auto move : node.getAllCandidateMoves()) {
-            Mover m(move, node);
-            int prevAlpha = alpha;
+    if (moves.size() == 0)
+        return node.getNextMoveColour() == Colour::WHITE ? -1024 : 1024;
 
-            val = std::max(val, alphaBeta(node, depth - 1,
-                                          alpha, beta, false, nullptr));
+    int val = (type == MinMax::MAX ? -INT_MAX : INT_MAX);
+
+    for (const auto move : moves) {
+        Mover m(move, node);
+        int prevVal = val;
+
+        if (type == MinMax::MAX) {
+            val = std::max(val, alphaBeta<MinMax::MIN>(node, depth - 1,
+                                                       alpha, beta, nullptr));
             alpha = std::max(alpha, val);
-
-            if (bestMove && alpha != prevAlpha)
-                *bestMove = move;
-
-            if (alpha >= beta)
-                break;
-        }
-        return val;
-    } else {
-        int val = INT_MAX;
-
-        for (const auto move : node.getAllCandidateMoves()) {
-            Mover m(move, node);
-            int prevBeta = beta;
-
-            val = std::min(val, alphaBeta(node, depth - 1,
-                                          alpha, beta, true, nullptr));
+        } else {
+            val = std::min(val, alphaBeta<MinMax::MAX>(node, depth - 1,
+                                                       alpha, beta, nullptr));
             beta = std::min(beta, val);
-
-            if (bestMove && beta != prevBeta)
-                *bestMove = move;
-
-            if (alpha >= beta)
-                break;
         }
-        return val;
+
+        if (bestMove && prevVal != val)
+            *bestMove = move;
+
+        if (alpha >= beta)
+            break;
     }
+
+    return val;
 }
 
 Move searchMove(Board &b, size_t depth)
 {
     int alpha = -INT_MAX;
     int beta = INT_MAX;
-
     Move m;
 
-    alphaBeta(b, depth, alpha, beta,
-              b.getNextMoveColour() == Colour::WHITE ? true : false,
-              &m);
+    auto searchFunc = b.getNextMoveColour() == Colour::WHITE ?
+        alphaBeta<MinMax::MAX> : alphaBeta<MinMax::MIN>;
+
+    searchFunc(b, depth, alpha, beta, &m);
 
     return m;
 }
