c16c498e81fc59b827df7dc6d01eb37d2b5de103
Matthew Leach <matthew@mattleach.net>
2018-12-22 15:17:19 +0000

    mace: use UCI
    Make the main program use the UCI protocol.

--- src/mace.cpp
+++ src/mace.cpp
@@ -1,43 +1,12 @@
 #include <iostream>
-#include "board.h"
-#include "piece.h"
-#include "search.h"
-#include "mover.hpp"
 
-void play(Board &b)
-{
-    b.printBoard(std::cout);
-
-    for (const auto move : b.getAllCandidateMoves())
-        std::cout << "Move: " << move << "\n";
-
-    std::string from;
-    std::string to;
-    std::cout << "From: ";
-    std::cin >> from;
-    if (from == "exit")
-        return;
-    std::cout << "To: ";
-    std::cin >> to;
-    std::cout << "\n";
-
-    auto m = b.validateMove(from, to);
-
-    if (!m.isValid()) {
-        std::cout << "Invalid move\n";
-        play(b);
-    }
-
-    Mover a(m, b);
-
-    play(b);
-}
+#include "uci.h"
 
 int main()
 {
-    Board b = Board::getStartingBoard();
+    UCI uci(std::cin, std::cout);
 
-    play(b);
+    uci.mainLoop();
 
     return 0;
 }
