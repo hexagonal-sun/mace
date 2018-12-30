3ce8bf5dc80a292ccbbceea411dfe5988bf7d2e9
Matthew Leach <matthew@mattleach.net>
2018-12-30 20:30:21 +0000

    evaluate: implement tapered phasing
    Implement tapered phasing and use PSTs that have different phases to
    them.

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -1,51 +1,153 @@
 #include "evaluate.h"
 
-static const std::array<int, 64> pawnTable =
+using psqt_t = std::array<std::array<int, 64>, 2>;
+
+static const psqt_t pstPawn =
 {
-    0,  0,  0,   0,   0,   0,   0,  0,
-    5,  10, 10,  -25, -25, 10,  10, 5,
-    5,  -5, -10, 0,   0,   -10, -5, 5,
-    0,  0,  0,   25,  25,  0,   0,  0,
-    5,  5,  10,  27,  27,  10,  5,  5,
-    10, 10, 20,  30,  30,  20,  10, 10,
-    50, 50, 50,  50,  50,  50,  50, 50,
-    0,  0,  0,   0,   0,   0,   0,  0,
+    // Middlegame
+    {{
+//A1                                H1
+        0,   0,   0,   0,   0,   0,   0,   0,
+        10,  20,  0,   5,   5,   0,   20,  10,
+        -10, 0,   5,   15,  15,  5,   0,   -10,
+        -15, -5,  10,  25,  25,  10,  -5,  -15,
+        -15, -5,  5,   15,  15,  5,   -5,  -15,
+        45,  55,  65,  75,  75,  65,  55,  45,
+        105, 115, 125, 135, 135, 125, 115, 105,
+        0,   0,   0,   0,   0,   0,   0,   0,
+//A8                                H8
+    },
+    // Endgame
+    {
+        0,   0,   0,   0,   0,   0,   0,   0,
+        3,   1,   -1,  -3,  -3,  -1,  1,   3,
+        3,   1,   -1,  -3,  -3,  -1,  1,   3,
+        9,   6,   3,   0,   0,   3,   6,   9,
+        12,  9,   6,   3,   3,   6,   9,   12,
+        87,  85,  83,  81,  81,  83,  85,  87,
+        159, 157, 155, 153, 153, 155, 157, 159,
+        0,   0,   0,   0,   0,   0,   0,   0,
+    }}
 };
 
-static const std::array<int, 64> knightTable =
+static const psqt_t pstKnight  =
 {
-    -50, -40, -20, -30, -30, -20, -40, -50,
-    -40, -20, 0,   5,   5,   0,   -20, -40,
-    -30, 5,   10,  15,  15,  10,  5,   -30,
-    -30, 0,   15,  20,  20,  15,  0,   -30,
-    -30, 5,   15,  20,  20,  15,  5,   -30,
-    -30, 0,   10,  15,  15,  10,  0,   -30,
-    -40, -20, 0,   0,   0,   0,   -20, -40,
-    -50, -40, -30, -30, -30, -30, -40, -50,
+    {{
+        -50, -40, -30, -25, -25, -30, -40, -50,
+        -35, -25, -15, -10, -10, -15, -25, -35,
+        -20, -10,   0,   5,   5,   0, -10, -20,
+        -10,   0,  10,  15,  15,  10,   0, -10,
+        -5,   5,  15,  20,  20,  15,   5,  -5,
+        -5,   5,  15,  20,  20,  15,   5,  -5,
+        -20, -10,   0,   5,   5,   0, -10, -20,
+        -135, -25, -15, -10, -10, -15, -25, -135
+    },
+    {
+        -40, -30, -20, -15, -15, -20, -30, -40,
+        -30, -20, -10,  -5,  -5, -10, -20, -30,
+        -20, -10,   0,   5,   5,   0, -10, -20,
+        -15,  -5,   5,  10,  10,   5,  -5, -15,
+        -15,  -5,   5,  10,  10,   5,  -5, -15,
+        -20, -10,   0,   5,   5,   0, -10, -20,
+        -30, -20, -10,  -5,  -5, -10, -20, -30,
+        -40, -30, -20, -15, -15, -20, -30, -40
+    }}
 };
 
-static const std::array<int, 64> bishopTable =
+static const psqt_t pstBishop =
 {
-    -20, -10, -40, -10, -10, -40, -10, -20,
-    -10, 5,   0,   0,   0,   0,   5,   -10,
-    -10, 10,  10,  10,  10,  10,  10,  -10,
-    -10, 0,   10,  10,  10,  10,  0,   -10,
-    -10, 5,   5,   10,  10,  5,   5,   -10,
-    -10, 0,   5,   10,  10,  5,   0,   -10,
-    -10, 0,   0,   0,   0,   0,   0,   -10,
-    -20, -10, -10, -10, -10, -10, -10, -20,
+    {{
+        -2, -5, -16, -5, -5, -16, -5, -2,
+        -5, 6,  3,   6,  6,  3,   6,  -5,
+        -5, 5,  8,   10, 10, 8,   5,  -5,
+        -5, 0,  10,  13, 13, 10,  0,  -5,
+        -5, 0,  5,   13, 13, 5,   0,  -5,
+        -5, 0,  5,   5,  5,  5,   0,  -5,
+        -5, 0,  0,   0,  0,  0,   0,  -5,
+        -5, -5, -5,  -5, -5, -5,  -5, -5
+    },
+    {
+        -15, -10,  -8,  -5,  -5,  -8, -10, -15,
+        -10,  -8,   0,   5,   5,   0,  -8, -10,
+        -5,   0,   5,  10,  10,   5,   0,  -8,
+        -5,   5,  10,  15,  15,  10,   5,  -5,
+        -5,   5,  10,  15,  15,  10,   5,  -5,
+        -8,   0,   5,  10,  10,   5,   0,  -8,
+        -10,  -8,   0,   5,   5,   0,  -8, -10,
+        -15, -10,  -8,  -5,  -5,  -8, -10, -15
+    }}
 };
 
-static const std::array<int, 64> kingTable =
+static const psqt_t pstRook =
 {
-    20,  30,  10,  0,   0,   10,  30,  20,
-    20,  20,  0,   0,   0,   0,   20,  20,
-    -10, -20, -20, -20, -20, -20, -20, -10,
-    -20, -30, -30, -40, -40, -30, -30, -20,
-    -30, -40, -40, -50, -50, -40, -40, -30,
-    -30, -40, -40, -50, -50, -40, -40, -30,
-    -30, -40, -40, -50, -50, -40, -40, -30,
-    -30, -40, -40, -50, -50, -40, -40, -30,
+    {{
+        0,  1,  2,  4,  4,  2,  1,  0,
+        -4, -2, 0,  2,  2,  0,  -2, -4,
+        -4, -2, 0,  2,  2,  0,  -2, -4,
+        -4, -2, 0,  2,  2,  0,  -2, -4,
+        -4, -2, 0,  2,  2,  0,  -2, -4,
+        -4, -2, 0,  2,  2,  0,  -2, -4,
+        15, 15, 15, 15, 15, 15, 15, 15,
+        4,  4,  4,  4,  4,  4,  4,  4
+    },
+    {
+        0,   0,   0,   0,   0,   0,   0,   0,
+        -2,   0,   0,   0,   0,   0,   0,  -2,
+        -2,   0,   0,   0,   0,   0,   0,  -2,
+        -2,   0,   0,   0,   0,   0,   0,  -2,
+        -2,   0,   0,   0,   0,   0,   0,  -2,
+        -2,   0,   0,   0,   0,   0,   0,  -2,
+        15,  15,  15,  15,  15,  15,  15,  15,
+        4,   4,   4,   4,   4,   4,   4,   4
+    }}
+};
+
+static const psqt_t pstQueen =
+{
+    {{
+            -5, -5, -5, -5, -5, -5, -5, -5,
+            0,  0,  0,  0,  0,  0,  0,  0,
+            0,  0,  0,  0,  0,  0,  0,  0,
+            0,  0,  0,  0,  0,  0,  0,  0,
+            0,  0,  0,  0,  0,  0,  0,  0,
+            0,  0,  0,  0,  0,  0,  0,  0,
+            0,  0,  0,  0,  0,  0,  0,  0,
+            0,  0,  0,  0,  0,  0,  0,  0
+        },
+     {
+         -24, -16, -12, -8, -8, -12, -16, -24,
+         -16, -12, -4,  0,  0,  -4,  -12, -16,
+         -12, -4,  0,   4,  4,  0,   -4,  -12,
+         -8,  0,   4,   8,  8,  4,   0,   -8,
+         -8,  0,   4,   8,  8,  4,   0,   -8,
+         -12, -4,  0,   4,  4,  0,   -4,  -12,
+         -16, -12, -4,  0,  0,  -4,  -12, -16,
+         -24, -16, -12, -8, -8, -12, -16, -24
+     }}
+};
+
+static const psqt_t pstKing =
+{
+    {{
+        40,  50,  30,  10,  10,  30,  50,  40,
+        30,  40,  20,  0,   0,   20,  40,  30,
+        10,  20,  0,   -20, -20, 0,   20,  10,
+        0,   10,  -10, -30, -30, -10, 10,  0,
+        -10, 0,   -20, -40, -40, -20, 0,   -10,
+        -20, -10, -30, -50, -50, -30, -10, -20,
+        -30, -20, -40, -60, -60, -40, -20, -30,
+        -40, -30, -50, -70, -70, -50, -30, -40
+    },
+    {
+        -72, -48, -36, -24, -24, -36, -48, -72,
+        -48, -24, -12, 0,   0,   -12, -24, -48,
+        -36, -12, 0,   12,  12,  0,   -12, -36,
+        -24, 0,   12,  24,  24,  12,  0,   -24,
+        -24, 0,   12,  24,  24,  12,  0,   -24,
+        -36, -12, 0,   12,  12,  0,   -12, -36,
+        -48, -24, -12, 0,   0,   -12, -24, -48,
+        -72, -48, -36, -24, -24, -36, -48, -72
+    }}
 };
 
 static int colourCorrectMagnitude(int magnitude, Colour c)
@@ -57,7 +159,8 @@ static int colourCorrectMagnitude(int magnitude, Colour c)
 }
 
 static int getValueFromTable(Locus l, SquareState s,
-                             const std::array<int, 64> &table)
+                             const psqt_t &table,
+                             int phase)
 {
     int idx = l.getIndex();
 
@@ -68,23 +171,32 @@ static int getValueFromTable(Locus l, SquareState s,
         tableIndex = (tableIndex & 7) |
                      ((7 << 3) - (tableIndex & (7 << 3)));
 
-    return colourCorrectMagnitude(table[tableIndex], s.getColour());
+    int openingEval = table[0][tableIndex];
+    int endgameEval = table[1][tableIndex];
+    int eval = ((openingEval * (256 - phase)) +
+                (endgameEval * phase)) / 256;
+
+    return colourCorrectMagnitude(eval, s.getColour());
 }
 
-static int getPSQTValue(Locus l, SquareState s)
+static int getPSQTValue(Locus l, SquareState s, int phase)
 {
     auto pieceType = s.getPieceType();
 
     switch (pieceType)
     {
     case PieceType::PAWN:
-        return getValueFromTable(l, s, pawnTable);
+        return getValueFromTable(l, s, pstPawn, phase);
     case PieceType::KNIGHT:
-        return getValueFromTable(l, s, knightTable);
+        return getValueFromTable(l, s, pstKnight, phase);
     case PieceType::BISHOP:
-        return getValueFromTable(l, s, bishopTable);
+        return getValueFromTable(l, s, pstBishop, phase);
     case PieceType::KING:
-        return getValueFromTable(l, s, kingTable);
+        return getValueFromTable(l, s, pstKing, phase);
+    case PieceType::QUEEN:
+        return getValueFromTable(l, s, pstQueen, phase);
+    case PieceType::ROOK:
+        return getValueFromTable(l, s, pstRook, phase);
     default:
         return 0;
     }
@@ -115,9 +227,40 @@ static int getPieceValue(SquareState s)
                                   s.getColour());
 }
 
+int calcuatePhase(Board &b)
+{
+    static const std::map<PieceType, int> phaseWeights =
+    {
+        { PieceType::KING,   0 },
+        { PieceType::PAWN,   0 },
+        { PieceType::KNIGHT, 1 },
+        { PieceType::BISHOP, 1 },
+        { PieceType::ROOK,   2 },
+        { PieceType::QUEEN,  4 }
+    };
+
+    const int totalPhase = phaseWeights.at(PieceType::PAWN) * 16 +
+        phaseWeights.at(PieceType::KNIGHT) * 4 +
+        phaseWeights.at(PieceType::BISHOP) * 4 +
+        phaseWeights.at(PieceType::ROOK) * 4 +
+        phaseWeights.at(PieceType::QUEEN) * 2;
+
+    int phase = totalPhase;
+
+    for (const auto pieceType : pieceTypes) {
+        auto count = b.pieceCounts.at(pieceType);
+
+        phase -= phaseWeights.at(pieceType) * count;
+    }
+
+    return (phase * 256 + (totalPhase / 2)) / totalPhase;
+}
+
 int getEvaluation(Board &b)
 {
     int eval = 0;
+
+    int phase = calcuatePhase(b);
 
     for (const auto [sq, loc] : b.getChessBoard())
     {
@@ -125,7 +268,7 @@ int getEvaluation(Board &b)
             continue;
 
         eval += getPieceValue(sq);
-        eval += getPSQTValue(loc, sq);
+        eval += getPSQTValue(loc, sq, phase);
     }
 
     return eval;
